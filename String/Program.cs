3e1e0f06cec292db41b30df98a444f868845c37e
Lucas <lucastisuporte@gmail.com>
2023-11-27 08:18:07 -0300

    Condeudo curço csharp

---
+++ String/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace String_Sharp {
+
+class String_Sharp{
+    static void Main(string[] args){
+      
+      string nome = "Lucas Serafim Cordeiro";
+      string idade = "25"
+        
+
+    }
+}
+
+}

e652d3f951d3ab37ec409b67e44e4fd9d040e8ff
Lucas Cordeiro <lucas.cordeiro@totvs.com.br>
2024-01-09 08:40:53 -0300

    Interface produto

---
+++ OrientecaoObjeto/ListaExercicios/03GestaoEstoque/03GestaoEstoque/IProduto.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03GestaoEstoque
+{
+    public  interface  IProduto
+    {
+        public abstract string Setor { get; }
+    }
+}
--- OrientecaoObjeto/ListaExercicios/03GestaoEstoque/03GestaoEstoque/Produtos.cs
+++ OrientecaoObjeto/ListaExercicios/03GestaoEstoque/03GestaoEstoque/Produtos.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace Ex03GestaoEstoque
 {
-    public class Produto
+    public class Produto : IProduto
     {
         public string Descricao { get; set; }
         public double Preco { get; set; }
         public int Estoque { get; set; }
+
+        // interface 
+        public override string Setor {get; set;}
     }
 
 }

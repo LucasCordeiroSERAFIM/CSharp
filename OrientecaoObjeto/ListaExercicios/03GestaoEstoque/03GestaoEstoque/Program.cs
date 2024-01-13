1867064b3017fbcf5b89704fdfd4c2d3cf77ff15
Lucas <lucastisuporte@gmail.com>
2024-01-13 10:55:02 -0300

    Operações produto

--- OrientecaoObjeto/ListaExercicios/03GestaoEstoque/03GestaoEstoque/Program.cs
+++ OrientecaoObjeto/ListaExercicios/03GestaoEstoque/03GestaoEstoque/Program.cs
@@ -6,6 +6,8 @@ namespace Ex03GestaoEstoque
     {
         public static void Main(string[] args)
         {
+
+            // *********************Clientes*********************
             // Ciente padrão
             ClientePadrao clientePadrao = new ClientePadrao();
             clientePadrao.Nome = "Nome Padrão";
@@ -17,17 +19,39 @@ namespace Ex03GestaoEstoque
             clienteespecial.Id = 2;
             clienteespecial.Nome = "Nome Especial";
             clienteespecial.Endereco = "José gabriotti 583";
-            
+
+            // *********************Produtos*********************
+            // Produtos Enlatados
+            Enlatados AlmondegaLata = new Enlatados();
+            AlmondegaLata.Descricao = "Almondegas en lata";
+            AlmondegaLata.Estoque = 25;
+            AlmondegaLata.Valor = 2.50;
+
+            // Produtos laticvinios
+            Laticinio leite = new Laticinio();
+            leite.Descricao = "Leite Longa vida Uht";
+            leite.Estoque = 2;
+            leite.Valor = 15.00;
+
+            // Produtos bebidas
+            Bebida cocaColaLitro = new Bebida();
+            cocaColaLitro.Descricao = "Coca cola retornavel 1 litro";
+            cocaColaLitro.Estoque = 400;
+            cocaColaLitro.Valor = 6.50;
             
             // gestão
             Gestao gestao = new Gestao();
-
+            // cadastrar clientes
             gestao.CadastrarCliente(clientePadrao);
-
             gestao.CadastrarCliente(clienteespecial);
 
-            // listar clientes 
-            gestao.ListarCliente();
+            // cadastrar produtos
+            gestao.CadastrarProduto(leite);
+            gestao.CadastrarProduto(cocaColaLitro);
+            gestao.CadastrarProduto(AlmondegaLata);
+            // listar clientes e produtos
+            //gestao.ListarCliente();
+            gestao.ListarProdutos();
         }
     }
 }
